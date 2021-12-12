7fa22f51d80d50d8c2de80147f3862839466a3ce
FedericoMoretto <moretto.federico@tecnest.it>
2021-12-12 17:35:24 +0100

    change homepage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router,Routes,Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Running from './pages/Running';
-import initialize from './local/initilization';
-import Trekking from './pages/Trekking';
-import Climbing from './pages/Climbing';
-import Fitness from './pages/Fitness';
-import Settings from './pages/Settings';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Running from "./pages/Running";
+import initialize from "./local/initilization";
+import Trekking from "./pages/Trekking";
+import Climbing from "./pages/Climbing";
+import Fitness from "./pages/Fitness";
+import Settings from "./pages/Settings";
 
 var moment = require("moment");
 var momentDurationFormatSetup = require("moment-duration-format");
@@ -18,10 +18,9 @@ momentDurationFormatSetup(moment);
 
 initialize();
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <Router >
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/running" element={<Running />} />
@@ -32,7 +31,7 @@ ReactDOM.render(
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
